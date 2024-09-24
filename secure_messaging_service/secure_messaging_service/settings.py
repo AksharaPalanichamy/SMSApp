159112f25b1c3f141e37e427242569981d7ea01c
AksharaPalanichamy <111784358+AksharaPalanichamy@users.noreply.github.com>
2024-09-24 12:06:56 +0530

    Update settings.py

--- secure_messaging_service/secure_messaging_service/settings.py
+++ secure_messaging_service/secure_messaging_service/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'secure_message_service_db',
         'USER': 'application_user',
-        'PASSWORD': 'Akshara123!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
